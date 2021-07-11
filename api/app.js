4c1a213fe0730ef2f996e1d2d3b3ec493b03a54d
Manuel Ahumada <mahumada@valtek.cl>
2021-07-11 00:42:35 -0400

    Se agrega codigo para realizar busquedas dinamicas al portal wikidata desde el boton buscar

--- api/app.js
+++ api/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+var fetch = require('node-fetch');
+var wbk = require('wikidata-sdk');
+var md5 = require('blueimp-md5'); 
 
 const app = express()
 const root = path.resolve(__dirname, '..')
@@ -17,5 +20,77 @@ app.get('/api/entities', (req,res) =>
     'Q33986'
    ]})
 );
+
+
+app.get('/entidades/:txtEntidad', function(req, response){  
+
+  var entidad = req.params.txtEntidad;
+  const urlentidad = wbk.searchEntities(entidad);   
+ 
+  fetch(urlentidad)
+      .then(res => res.json())
+      .then(json => {
+          var idsEntidades = [];
+          for (var i = 0, fila; fila = json.search[i]; i++) {
+              idsEntidades.push(fila.id);
+          }
+
+          const urlids = wbk.getEntities({
+              ids: idsEntidades,
+              languages: [ 'es' ]
+            });
+          
+          fetch(urlids)
+            .then(resids => resids.json())
+            .then(wbk.parse.wb.entities)
+            .then(entities => {
+
+              var listacompuesta = [];
+              
+              for (var propertyName in entities) {
+                var listadoPropiedades = entities[propertyName];
+                var descripcionPropiedad = (listadoPropiedades.descriptions.es === undefined ? '-' : listadoPropiedades.descriptions.es);
+                var labelPropiedad = (listadoPropiedades.labels.es === undefined ? 'Sin Información' : listadoPropiedades.labels.es);
+                var urlImagen;
+                for (var propertyTopic in listadoPropiedades.claims) {                  
+
+                  var nombrePais;
+                  if (propertyTopic == 'P17') {
+                    var objeto = listadoPropiedades.claims[propertyTopic];
+                    nombrePais = objeto[0];
+                  }
+
+                  if (propertyTopic == 'P18') {
+                    var objeto = listadoPropiedades.claims[propertyTopic];
+                    var nombreImagen = objeto[0];
+                    nombreImagen = nombreImagen.split(' ').join('_')
+                    var md5sum = md5(nombreImagen);
+                    var a = md5sum[0];
+                    var b = md5sum[1];
+                    urlImagen = 'https://upload.wikimedia.org/wikipedia/commons/' + a + '/' + a + '' + b + '/' + nombreImagen;
+                  }
+                }
+
+                if (descripcionPropiedad != '-') {
+                  console.log('(' + propertyName + ') ' + descripcionPropiedad + ' - ' + labelPropiedad + '  = URL(' + (urlImagen === undefined?'-': urlImagen) + ')');
+                  var item = {
+                    idtopico: propertyName,
+                    descripcion: descripcionPropiedad,
+                    label: labelPropiedad,
+                    urlimagen: urlImagen
+                  };
+
+                  urlImagen = undefined;
+                  listacompuesta.push(item);
+                  
+                }
+              }
+
+              response.send(listacompuesta);
+    
+            });
+      });
+});
+
 
 module.exports = app
--- client/code.js
+++ client/code.js
@@ -1,18 +1,54 @@
-async function getEntities() {
-    const response = await fetch('/api/entities');
-    const data = await response.json();
-    return data
+async function getEntities(entidad) {
+    const response = await fetch('/entidades/'+ entidad)
+    .then(res => res.json())
+    .then(json => {
+        //debugger;
+        var contenedor = document.getElementById('divContenedor');
+        contenedor.innerHTML = '';
+        for (var i = 0, fila; fila = json[i]; i++) {
+            var div = document.createElement('div');
+            if(i % 2 === 0)            
+            {
+                div.innerHTML = `
+                <hr class="featurette-divider">
+                <div class="row featurette">
+                    <div class="col-md-7">
+                        <h2 class="featurette-heading">`+fila.descripcion+`</h2>
+                        <p class="lead">`+fila.label+`</p>
+                        <a href="https://www.wikidata.org/wiki/`+fila.idtopico+`">WikiData (`+fila.idtopico+`)</a>
+                    </div>
+                    <div class="col-md-5">            
+                        <img src="`+(fila.urlimagen === undefined ? 'https://image.freepik.com/vector-gratis/pagina-error-404-no-encontrada_41910-364.jpg': fila.urlimagen )+`" width="500" height="500" />        
+                    </div>
+                </div>            
+                            `;
+            }
+            else
+            {
+
+                div.innerHTML = `
+                <hr class="featurette-divider">
+                <div class="row featurette">
+                    <div class="col-md-7 order-md-2">
+                        <h2 class="featurette-heading">`+fila.descripcion+`</h2>
+                        <p class="lead">`+fila.label+`</p>
+                        <a href="https://www.wikidata.org/wiki/`+fila.idtopico+`">WikiData (`+fila.idtopico+`)</a>
+                    </div>
+                    <div class="col-md-5 order-md-1">            
+                        <img src="`+(fila.urlimagen === undefined ? 'https://image.freepik.com/vector-gratis/pagina-error-404-no-encontrada_41910-364.jpg': fila.urlimagen )+`" width="500" height="500" />
+                    </div>
+                </div>          
+                            `;
+            }
+            
+            contenedor.appendChild(div);
+        }
+    });
 }
 
-function fillEntities() {
-    getEntities().then(data => {
-        console.log(data.entities);
-        const ulEntities = document.getElementById("entities");
-        data.entities.forEach(entity => {
-          const liEntity = document.createElement("li");
-          const text = document.createTextNode(entity);
-          liEntity.appendChild(text);
-          ulEntities.appendChild(liEntity);
-        })
-    })
+
+function buscarEntidades(){    
+    var txt = document.getElementById('txtBuscar');    
+    getEntities(txt.value);
+    console.log("Procesando resultados!!");
 }
